bf2993b2f678567ee8204fd3954c53a09055d662
Vinod <raita0100@gmail.com>
2019-10-10 17:02:43 +0530

    Integrated stripe payment

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import pyrebase
 from flask import *
 import json
 import prediction_engine.Stock_Direction as sd
+#importing payment library
+import stripe
+
+#setting the stripe variables
+STRIPE_PUBLISHABLE_KEY = 'pk_test_9qnorLWUQILDlpMbGFjx1jM300ESwEvbGe'
+STRIPE_SECRET_KEY = 'sk_test_AxJs3AsRcXNSw5kZ6RUr66Au00LLXE8ynD'
+
+stripe.api_key = STRIPE_SECRET_KEY
+
 #defining the firebase configurations from created app int he firebase
 config = {
     "apiKey": "AIzaSyDr-4uZw1snFiOg26eQC7VOsvUPYCB3zVU",
@@ -117,16 +126,18 @@ def create_user():
 @app.route("/logout_")
 def logout():
     auth.current_user=None
+    dict_of_items = None
     return redirect(url_for("index", msg="You have been logged out"))
 
 #route for selector
 @app.route("/selector")
-def selector():
+@app.route("/selector<msg>")
+def selector(msg=None):
     global _companies
     curr_user = auth.current_user
     if curr_user == None:
         return redirect(url_for('index', msg='login to continue'))
-    return render_template('selector.html', itms=_companies)
+    return render_template('selector.html', itms=_companies, msg=msg)
 
 #route for processing the selected list
 @app.route("/submit_items", methods = ['GET','POST'])
@@ -150,7 +161,15 @@ def predictor():
     curr_user = auth.current_user
     if curr_user == None:
         return redirect(url_for('index', msg='login to continue'))
-    
+
+    if db.child("users").child(info['users'][0]['localId']).child("paid").get().val() == 0:
+        return redirect(url_for("pay_money", msg="Please pay to continue"))
+
+    if dict_of_items == None or not dict_of_items:
+        return redirect(url_for('selector', msg="please select 5 stock to continue"))
+    #get the payment recipt link
+    recipt = db.child("users").child(info['users'][0]['localId']).child("payment").child("receipt_url").get().val()
+
     x = {}
     for i in dict_of_items.keys():
         x[dict_of_items[i]] = _companies[dict_of_items[i]]
@@ -160,7 +179,59 @@ def predictor():
     uname = db.child("users").child(info['users'][0]['localId']).child('name').get()
     uname = uname.val()
 
-    return render_template('predict.html', msg=uname, s_list=x, pred=dict(f_pred))
+    return render_template('predict.html', msg=uname, s_list=x, pred=dict(f_pred), rec=recipt)
+
+#payment methods
+@app.route("/pay_money")
+@app.route("/pay_money<msg>")
+def pay_money(msg=None):
+    #check for user login
+    curr_user = auth.current_user
+    if curr_user == None:
+        return redirect(url_for('index', msg='login to continue'))
+
+    if db.child("users").child(info['users'][0]['localId']).child("paid").get().val() == 1:
+        return redirect(url_for("selector", msg="you have paid"))
+
+    return render_template('payment.html', msg=msg)
+
+@app.route("/pay", methods=['POST'])
+def pay():
+
+    #creating the cusromer and chage in stripe by paid user
+    amount = 25000
+
+    try:
+        customer = stripe.Customer.create(
+            email=request.form['stripeEmail'],
+            source=request.form['stripeToken']
+        )
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency='USD',
+            customer=customer.id,
+            description='payment for Fire Trade'
+        )
+
+        #saving data in firebase
+        p_data = {
+            "paid":charge["paid"],
+            "amount":charge["amount"],
+            "paid_mail":request.form['stripeEmail'],
+            "currency":charge["currency"],
+            "customer":charge["customer"],
+            "description":charge["description"],
+            "payment_method_details":charge["payment_method_details"],
+            "receipt_url":charge["receipt_url"],
+            "status":charge["status"]
+        }
+        db.child("users").child(info['users'][0]['localId']).update({"paid":1})
+        db.child("users").child(info['users'][0]['localId']).child("payment").set(p_data)
+        return redirect(url_for("predictor"))
+
+    except Exception as e:
+        print(str(e))
+        return redirect(url_for('selector', msg='payment failed please try again'))
 
 if __name__ == "__main__":
     app.debug = True
